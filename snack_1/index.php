101b72f8759a34459f494dff6bf9416ef128bc4d
MartinaGangemi <martinagangemi94@gmail.com>
2022-05-20 12:42:43 +0200

    initial commit

---
+++ snack_1/index.php
@@ -0,0 +1,39 @@
+<?php
+    $array = [
+        [
+            "casa" => "Milano",
+            "ospite" => "Brindisi",
+            "puntiCasa" => 80,
+            "puntiOspite" => 60,
+        ],
+        [
+            "casa" => "Torino",
+            "ospite" => "Roma",
+            "puntiCasa" => 50,
+            "puntiOspite" => 65,
+        ],
+        [
+            "casa" => "Asti",
+            "ospite" => "Monza",
+            "puntiCasa" => 80,
+            "puntiOspite" => 60,
+        ],
+    ];
+
+    var_dump($array)
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p></p>
+</body>
+</html>
